9839f9bace785634f16e0d914585ed1dd35455ae
anthonin-tech <anthonin.chesneau08@gmail.com>
2025-12-18 09:43:03 +0100

    commencement de la page profil

---
+++ views/pages/profil.php
@@ -0,0 +1,35 @@
+<link rel="stylesheet" href="assets/css/profil.css">
+
+<section class="box_presentation box ">
+    <div class="container">
+        <div class="présentation">
+            <h1>Qui je suis ?</h1>
+            <p>Bonjour à tous ! Je m’appelle Anthonin Chesneau, j’ai 18 ans et je suis né en France.</p><br>
+            <p>Je poursuis actuellement un Bachelor en Systèmes Informatiques et Numériques (SIN) ainsi qu’un BTS SIO (Services Informatiques aux Organisations), formations qui me permettent de développer de solides compétences en développement, en réseaux et en systèmes informatiques.</p><br>
+            <p>Mon objectif académique est de continuer mes études vers un Master spécialisé en intelligence artificielle, afin de participer à la conception de solutions innovantes et à fort impact technologique.</p><br>
+            <p>En dehors de mes études, j’aime explorer de nouvelles technologies, renforcer mes compétences techniques et m’investir dans des activités créatives qui nourrissent ma curiosité et mon envie d’apprendre.</p><br>
+        </div>
+
+        <ul class="passion">
+            <li>Jouer à des jeux 🎮</li>
+            <li>Pratiquer des activités sportifs, actuellement du rugby en club 🏉</li>
+            <li>Découvrir des choses 📝</li>
+            <li>Vivre des expériences 🏃🏼‍♀️</li>
+        </ul><br>
+
+        <p class="citation">« Le succès consiste à aller d’échec en échec sans perdre son enthousiasme. »
+        — Winston Churchill</p>
+    </div>
+</section>
+
+<section>
+    <h1>compétences professionnelles</h1>
+</section>
+
+<section>
+    <h1>Outils que j'utilise</h1>
+</section>
+
+<section>
+    <h1>Jour où je code</h1>
+</section>
--- views/partials/_navbar.php
+++ views/partials/_navbar.php
@@ -2,28 +2,28 @@
   <div class="navbar-menu">
     <div class="navbar-end">
 
-      <a class="navbar-item has-text-white mr-4 lien-nav" href="views/pages/home.php">
+      <a class="navbar-item has-text-white mr-4 lien-nav" href="index.php?page=home">
         <figure class="image is-24x24 mr-1">
           <img src="assets/illustration/icone-acceuil.png">
         </figure>
         Accueil
       </a>
 
-      <a class="navbar-item has-text-white mr-4 lien-nav" href="views/pages/about.php">
+      <a class="navbar-item has-text-white mr-4 lien-nav" href="index.php?page=profil">
         <figure class="image is-24x24 mr-1">
           <img src="assets/illustration/icone-profil.png">
         </figure>
         Profil
       </a>
 
-      <a class="navbar-item has-text-white mr-4 lien-nav" href="views/pages/Project.php">
+      <a class="navbar-item has-text-white mr-4 lien-nav" href="index.php?page=projets">
         <figure class="image is-24x24 mr-1">
           <img src="assets/illustration/icone-dossier.png">
         </figure>
         Projets
       </a>
 
-      <a class="navbar-item has-text-white mr-4 lien-nav" href="views/pages/Resume.php">
+      <a class="navbar-item has-text-white mr-4 lien-nav" href="index.php?page=cv">
         <figure class="image is-24x24 mr-1">
           <img src="assets/illustration/icone-cv.png">
         </figure>
